23f132cc2b742d86d88d8bccf007ddc5a568a997
shubhamg931 <sg270798@gmail.com>
2018-03-23 23:52:23 +0530

    initializing

---
+++ invinciblesHack/app/src/main/java/com/expledia/invincibles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.expledia.invincibles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
