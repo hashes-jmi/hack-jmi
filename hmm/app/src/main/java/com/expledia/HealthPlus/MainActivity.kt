c234856018b7f2958283d5cb042cdfeb1184c7bc
mkm1997 <mmanish184@gmail.com>
2018-03-24 18:40:10 +0530

    r

--- hmm/app/src/main/java/com/expledia/HealthPlus/MainActivity.kt
+++ hmm/app/src/main/java/com/expledia/HealthPlus/MainActivity.kt
@@ -18,10 +18,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
