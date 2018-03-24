074d9f3cae97a1e7fa42fd2f35f6a5398459466d
Abdul Wadood <abdulwd97@gmail.com>
2018-03-24 15:40:37 +0530

    Update MainActivity

--- Lifeline/app/src/main/java/com/abdulwd/lifeline/LoginActivity.java
+++ Lifeline/app/src/main/java/com/abdulwd/lifeline/LoginActivity.java
@@ -3,6 +3,7 @@ package com.abdulwd.lifeline;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 
@@ -19,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
--- Lifeline/app/src/main/java/com/abdulwd/lifeline/main/MainActivity.java
+++ Lifeline/app/src/main/java/com/abdulwd/lifeline/main/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setSupportActionBar(findViewById(R.id.toolbar));
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
