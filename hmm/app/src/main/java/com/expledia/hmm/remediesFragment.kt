29e25911bcdef175d4a3330bfc29c9cb421899e2
shubhamg931 <sg270798@gmail.com>
2018-03-24 15:27:06 +0530

    spinner working

--- hmm/app/src/main/java/com/expledia/hmm/remediesFragment.kt
+++ hmm/app/src/main/java/com/expledia/hmm/remediesFragment.kt
@@ -6,29 +6,69 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ListAdapter
 import com.toptoche.searchablespinnerlibrary.SearchableSpinner
+import io.apptik.widget.multiselectspinner.BaseMultiSelectSpinner
 import kotlinx.android.synthetic.main.remedies_fragment.view.*
+import io.apptik.widget.multiselectspinner.MultiSelectSpinner
+import java.util.ArrayList
+
 
 /**
  * Created by shubhamg931 on 24/3/18.
  */
 open class remediesFragment :android.support.v4.app.Fragment()
 {
-    lateinit var myView:View
-    lateinit var searchableSpinner: SearchableSpinner
-    var myArrayList= arrayOf("1,2","3","4","hello","hibro")
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var myView = View(context)
+        var ans=ArrayList<String>()
+        myView = LayoutInflater.from(context).inflate(R.layout.remedies_fragment, container, false)
+        var myAdapter:ArrayAdapter<String>
+        val options = ArrayList<String>()
+        options.add("fever")
+        options.add("headache")
+        options.add("rash")
+        options.add("nosebleed")
+        options.add("backpain")
+        options.add("diziness")
+        options.add("stomachache")
+        options.add("laziness")
+        options.add("cough")
+        options.add("sneeze")
+        myAdapter=ArrayAdapter<String>(context,android.R.layout.simple_list_item_1,ans)
+        myView.remedyList.adapter=myAdapter
+
+        var multiSelectSpinner1=myView.multiselectSpinner
+        multiSelectSpinner1.setItems(options)
+
+                .setListener<BaseMultiSelectSpinner>(object : BaseMultiSelectSpinner.MultiSpinnerListener {
+                    override fun onItemsSelected(selected: BooleanArray) {
+                        ans.clear()
+                        for (i in selected.indices) {
+                            if (selected[i] == true)
+                            {
+                                ans.add(options[i])
+                                myAdapter.notifyDataSetChanged()
+                            }
+
+                            if (selected[i] == false)
+                                try {
+                                    ans.remove(options[i])
+                                } catch (ex: Exception) {
+                                }
+                        }
+
+                    }
+
+                })
+                .setAllCheckedText<BaseMultiSelectSpinner>("All types")
+                .setAllUncheckedText<BaseMultiSelectSpinner>("none selected")
+                .setSelectAll<BaseMultiSelectSpinner>(false)
 
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View
-    {
-        myView= inflater?.inflate(R.layout.remedies_fragment,container,false)!!
-        var adapter: ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.simple_spinner_item, myArrayList)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        myView.spinner.adapter = adapter
+
         return myView!!
-
     }
-
-
 
 }
---
+++ hmm/app/src/main/java/com/expledia/hmm/remediesFragmentJ.java
@@ -0,0 +1,67 @@
+package com.expledia.hmm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+
+import io.apptik.widget.multiselectspinner.MultiSelectSpinner;
+
+/**
+ * Created by shubhamg931 on 24/3/18.
+ */
+
+public class remediesFragmentJ extends remediesFragment {
+
+//
+//    @NotNull
+//    @Override
+//    public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//
+//        View myView = new View(getContext());
+//        myView=LayoutInflater.from(getContext()).inflate(R.layout.remedies_fragment, container, false);
+////        final ArrayList<boolean[]> selectedSymptoms = new ArrayList<>();
+//        final ArrayList<Boolean> myboolean=new ArrayList<Boolean>();
+//        final ArrayList<String> ans = new ArrayList<>();
+//        final ArrayList<String> options = new ArrayList<>();
+//        options.add("1");
+//        options.add("2");
+//        options.add("3");
+//        options.add("A");
+//        options.add("B");
+//        options.add("C");
+//
+//        MultiSelectSpinner multiSelectSpinner1 = (MultiSelectSpinner) myView.findViewById(R.id.multiselectSpinner);
+//        multiSelectSpinner1.setItems(options)
+//
+//                .setListener(new MultiSelectSpinner.MultiSpinnerListener() {
+//                    @Override
+//                    public void onItemsSelected(boolean[] selected) {
+//                        myboolean.clear();
+//                        for (int i=0;i<selected.length;++i){
+//                            myboolean.add(selected[i]);
+//                            if(selected[i] == true)
+//                                ans.add(options.get(i));
+//
+//                        }
+//
+//                    }
+//                })
+//                .setAllCheckedText("All types")
+//                .setAllUncheckedText("none selected")
+//                .setSelectAll(false);
+//        ;
+//
+//
+//        ListView list = (ListView) myView.findViewById(R.id.remedyList);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(getContext());
+//        return myView;
+//    }
+}
